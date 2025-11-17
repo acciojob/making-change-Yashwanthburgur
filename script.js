d58c605f26b0efc6c2e9ee2d3b58fb2be979114a
Yashwanth M <byg2281999@gmail.com>
2025-11-17 06:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const makeChange = (c) => {
   // your name here
+  let amount = c;
+  let q = 0, d = 0, n = 0, p = 0;
+  
+  while (amount > 0) {
+    if (amount >= 25) {
+      amount -= 25;
+      q++;
+    } else if (amount >= 10) {
+      amount -= 10;
+      d++;
+    } else if (amount >= 5) {
+      amount -= 5;
+      n++;
+    } else {
+      amount -= 1;
+      p++;
+    }
+  }
+  
+  return { q, d, n, p };
 };
 
 // Do not the change the code below
